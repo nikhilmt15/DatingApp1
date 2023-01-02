07327eb016fa8617aa92043022c195c32ef96e06
nikhilmt15 <nikhilrocking98@gmai.com>
2023-01-02 15:42:01 +0530

    End of Section2

---
+++ API/Entitites/AppUser.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entitites
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
